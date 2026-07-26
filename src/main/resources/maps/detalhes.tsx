e6f0e7fa540d2f23d5e3dc78a3aa90b6244db7c4
Gustavo <gustavoliveira8188@gmail.com>
2026-07-26 16:03:41 -0300

    Improved resources organization

---
+++ src/main/resources/maps/detalhes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="detalhes" tilewidth="16" tileheight="16" tilecount="560" columns="40">
+ <image source="../../../Downloads/craftpix-net-695666-free-undead-tileset-top-down-pixel-art/Tiled_files/details.png" width="640" height="224"/>
+</tileset>

cc6c9b5afa8e32a4b99af5f0e20868e4b8642eb6
Gustavo <gustavoliveira8188@gmail.com>
2026-07-29 14:05:50 -0300

    fix duplicate "resources"

--- resources/maps/Objetos.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="Objetos" tilewidth="16" tileheight="16" tilecount="3600" columns="75">
- <image source="../../../Downloads/craftpix-net-695666-free-undead-tileset-top-down-pixel-art/Tiled_files/Objects.png" width="1200" height="768"/>
-</tileset>
